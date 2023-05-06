8d82dfc8653652d5a3692ca7b91961eef8c4f62e
ngvlong <130909111+ngvlong@users.noreply.github.com>
2023-05-06 22:06:02 +0700

    Update NhanVien.java

--- src/GUI/NhanVien.java
+++ src/GUI/NhanVien.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GUI;
 
 
-import DTO.AccountDTO;
+
+
 import DTO.NhanVienDTO;
+import DAO.NhanVienDAO;
+import BUS.NhanVienBUS;
+import DTO.ProductDTO;
 import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
 import component.ImageScale;
@@ -16,13 +16,18 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.KeyListener;
+
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.imageio.ImageIO;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
@@ -35,12 +40,19 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.border.LineBorder;
 
 /**
  *
  * @author NAME
  */
 public class NhanVien extends  JPanel implements MouseListener,ActionListener{
+    
+
     
     JTable tblnhanvien;
     
@@ -52,13 +64,15 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
     
     JLabel lblimg;
     
-    Label[] lblinfo = new Label[6];
+    
+    Label[] lblinfo = new Label[8];
     
     JDateChooser calendar;
     
-    String[] lblinfo_name = {"Mã nhân viên","Tên nhân viên","Địa chỉ","Số điện thoại","Ngày sinh","Giới tính"};
+    String[] lblinfo_name = {"Mã nhân viên","Tên nhân viên","Giới tính","Địa chỉ","Số điện thoại","Ngày sinh","Ngày Vào","Học Vấn"};
             
-    JTextField[] txtinfo = new JTextField[4] ;
+
+    JTextField[] txtinfo = new JTextField[8] ;
     
     JRadioButton rbtnam,rbtnu;
     
@@ -67,18 +81,30 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
     Color main_clr = new Color(150, 150, 220);
     Color hover_clr = new Color(140, 140, 200);
     
-    NhanVienDTO nhanvien = new NhanVienDTO();
+
+    NhanVienDAO nhanvien = new NhanVienDAO();
+    
+    ArrayList<NhanVienDTO> nvlist = new ArrayList<>();
+    
+    int index =0;
+    
+    NhanVienDTO nv1 = new NhanVienDTO();
+    
     
     public void initcomponent(NhanVienDTO a) throws IOException{
         
-        nhanvien = a;
-        
+
+     
+        nvlist = nhanvien.selectAll();
+        nv1 = nvlist.get(0);
+      
+                
         this.setLayout(new GridLayout(2,1));
         this.setBorder(new EmptyBorder(5,5,5,5));
         this.setOpaque(true);
         this.setBackground(null);
         
-        String[] column = {"Mã nhân viên","Tên nhân viên","Ngày sinh","Địa chỉ","Giới tính","Tuổi","Số điện thoại"};
+        String[] column = {"Mã nhân viên","Tên nhân viên","Giới tính","Số điện thoại","Ngày Vào"};
         
         Dimension txtinfo_dms = new Dimension(200,30);
         Font lblinfo_font = new Font("Times New Roman",Font.CENTER_BASELINE,20);
@@ -86,14 +112,18 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
         
         
         tblnhanvien = new JTable();
+        tblnhanvien.addMouseListener(this);
+        
         spnhanvien = new JScrollPane();
         model = new DefaultTableModel();
         
-        for(int i =0;i<6;i++){
+
+        for(int i =0;i<5;i++){
             model.addColumn(column[i]);
         }
         
         tblnhanvien.setModel(model);
+        showdata(nvlist);
         
         spnhanvien.setViewportView(tblnhanvien);
         
@@ -101,15 +131,29 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
         pnlinfor.setOpaque(true);
         
         pnlimg = new JPanel(new BorderLayout());
-        pnlimg.setBorder(new EmptyBorder(15,0,0,10));
-        pnlimg.setPreferredSize(new Dimension(300,310));
-        if(nhanvien.getImg()!=null){
-            String img_path = nhanvien.getImg();
-            ImageIcon img_employee = ImageScale.scale_employee_img(new ImageIcon(ImageIO.read(new File( img_path))));
-            lblimg = new JLabel(img_employee);
+
+        pnlimg.setBorder(new EmptyBorder(15,50,0,10));
+        pnlimg.setPreferredSize(new Dimension(260,270));
+        lblimg = new JLabel();
+        lblimg.setSize(220,270);
+        lblimg.setBorder(new LineBorder(new Color(99,99,99),1,true));
+
+        String s = "src\\img_employee\\nhanvien.jpg";
+        if(nv1.getImg().trim()!=null){
+            
+                 String img_path = nv1.getImg();
+                 ImageIcon icon = new ImageIcon(ImageIO.read(new File(img_path)));
+                 Image image = icon.getImage();
+                 Image imgScale = image.getScaledInstance(lblimg.getWidth(), lblimg.getHeight(),Image.SCALE_SMOOTH);
+                 ImageIcon scaledIcon = new ImageIcon(imgScale);
+                 lblimg.setIcon(scaledIcon);
         }
         else{
-            lblimg = new JLabel("ERRO!!");
+                     ImageIcon icon = new ImageIcon(ImageIO.read(new File(s)));
+                     Image image = icon.getImage();
+                     Image imgScale = image.getScaledInstance(lblimg.getWidth(), lblimg.getHeight(),Image.SCALE_SMOOTH);
+                     ImageIcon scaledIcon = new ImageIcon(imgScale);
+                     lblimg.setIcon(scaledIcon);
         }
         pnlimg.add(lblimg,BorderLayout.CENTER);
         
@@ -117,14 +161,15 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
         pnlinfo_detail.setPreferredSize(new Dimension(1060,310));
         pnlinfo_detail.setBorder(new EmptyBorder(30,50,30,0));
         
-        int y_pos = 70;
-        int x_pos = 50;
+
+        int y_pos = 50;
+        int x_pos = 20;
         int x_index = 0;
         int count = 0;
         
         for(int i = 0 ; i<lblinfo_name.length;i++){
             lblinfo[i] = new Label(lblinfo_name[i]);
-            lblinfo[i].setBounds((x_pos+(440*count)),y_pos,150,30);
+            lblinfo[i].setBounds((x_pos+(450*count)),y_pos,150,30);
             lblinfo[i].setAlignment(2);
             lblinfo[i].setFont(lblinfo_font);
             
@@ -132,11 +177,12 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
             
             if(i < txtinfo.length) {
                 txtinfo[i] = new JTextField();    
-                txtinfo[i].setBounds((x_pos+160+(440*count)),y_pos,200,30);
+                txtinfo[i].setBounds((x_pos+160+(450*count)),y_pos,170,30);
                 pnlinfo_detail.add(txtinfo[i]);
             }
 
-            if(i==1 || i==3){
+
+            if(i==1 || i==3 || i == 5){
                 count = 0;
                 y_pos += 50;
             }
@@ -145,34 +191,42 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
             }
 
         }
+
+//        calendar = new JDateChooser();
+//        calendar.setDateFormatString("dd/MM/yyyy");
+//        calendar.setBounds(210,170,200,30);
+//        pnlinfo_detail.add(calendar);
+//                
+//        pnlsex = new JPanel(new GridLayout(1, 2));
+//        pnlsex.setBounds(650,170,200,30);
+////        pnlsex.setOpaque(true);
+//        pnlsex.setBorder(new EmptyBorder(0,20,0,0));
+////        pnlsex.setBackground(main_clr);
+//        
+//        rbtnam = new JRadioButton();
+//        rbtnu = new JRadioButton();
+//        rbtnam.setText("Nam");
+//        rbtnu.setText("Nữ");
+//        rbtnam.setFont(lblinfo_font);
+//        rbtnu.setFont(lblinfo_font);
+//        
+//        rbtgroup = new ButtonGroup();
+//        rbtgroup.add(rbtnam);
+//        rbtgroup.add(rbtnu);
+//        
+//        pnlsex.add(rbtnam);
+//        pnlsex.add(rbtnu);
+//        
+//        pnlinfo_detail.add(pnlsex);
         
-        calendar = new JDateChooser();
-        calendar.setDateFormatString("dd/MM/yyyy");
-        calendar.setBounds(210,170,200,30);
-        pnlinfo_detail.add(calendar);
-                
-        pnlsex = new JPanel(new GridLayout(1, 2));
-        pnlsex.setBounds(650,170,200,30);
-//        pnlsex.setOpaque(true);
-        pnlsex.setBorder(new EmptyBorder(0,20,0,0));
-//        pnlsex.setBackground(main_clr);
-        
-        rbtnam = new JRadioButton();
-        rbtnu = new JRadioButton();
-        rbtnam.setText("Nam");
-        rbtnu.setText("Nữ");
-        rbtnam.setFont(lblinfo_font);
-        rbtnu.setFont(lblinfo_font);
-        
-        rbtgroup = new ButtonGroup();
-        rbtgroup.add(rbtnam);
-        rbtgroup.add(rbtnu);
-        
-        pnlsex.add(rbtnam);
-        pnlsex.add(rbtnu);
-        
-        pnlinfo_detail.add(pnlsex);
-        
+        txtinfo[0].setText(nv1.getMaNV());
+        txtinfo[1].setText(nv1.getTenNV());
+        txtinfo[2].setText(nv1.getGioiTinh());
+        txtinfo[3].setText(nv1.getDiaChi());
+        txtinfo[4].setText(nv1.getSDT());
+        txtinfo[5].setText(nv1.getNgaySinh());
+        txtinfo[6].setText(nv1.getNgayVao());
+        txtinfo[7].setText(nv1.getHocVan());
         
         pnlinfor.add(pnlimg);
         pnlinfor.add(pnlinfo_detail);
@@ -182,6 +236,61 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
         this.add(pnlinfor);
     }
 
+    
+        private void desplaydetails(int selectedRows){
+    }
+    
+    public JTable gettbl(){
+        return  this.tblnhanvien;
+    }
+    
+    public DefaultTableModel getModel(){
+        return  this.model; 
+    }
+
+    public void showdata(ArrayList<NhanVienDTO> list){
+        model.setRowCount(0);
+        for(NhanVienDTO nhv : list){
+            model.addRow(new Object[] {nhv.getMaNV(),nhv.getTenNV(),nhv.getGioiTinh(),nhv.getSDT(),nhv.getNgayVao()});
+            }
+        tblnhanvien.setModel(model);
+    }
+    public void selectitem(ArrayList<NhanVienDTO> list) throws IOException{
+                desplaydetails(tblnhanvien.getSelectedRow());
+                this.index = tblnhanvien.getSelectedRow();
+                NhanVienDTO a = list.get(index);
+//                lblimg.setSize(220,270);
+                String s = "src\\img_employee\\nhanvien.jpg";
+                if(a.getImg() != null){
+            
+                     String img_path = a.getImg();
+                     ImageIcon icon = new ImageIcon(ImageIO.read(new File(img_path)));
+                     Image image = icon.getImage();
+                     Image imgScale = image.getScaledInstance(lblimg.getWidth(), lblimg.getHeight(),Image.SCALE_SMOOTH);
+                     ImageIcon scaledIcon = new ImageIcon(imgScale);
+                     lblimg.setIcon(scaledIcon);
+                }
+                else{
+                   
+                     ImageIcon icon = new ImageIcon(ImageIO.read(new File(s)));
+                     Image image = icon.getImage();
+                     Image imgScale = image.getScaledInstance(lblimg.getWidth(), lblimg.getHeight(),Image.SCALE_SMOOTH);
+                     ImageIcon scaledIcon = new ImageIcon(imgScale);
+                     lblimg.setIcon(scaledIcon);
+                 }
+                
+                 txtinfo[0].setText(a.getMaNV());
+                 txtinfo[1].setText(a.getTenNV());
+                 txtinfo[2].setText(a.getGioiTinh());
+                 txtinfo[3].setText(a.getDiaChi());
+                 txtinfo[4].setText(a.getSDT());
+                 txtinfo[5].setText(a.getNgaySinh());
+                 txtinfo[6].setText(a.getNgayVao());
+                 txtinfo[7].setText(a.getHocVan());    
+    }
+    public void setNhanvienlist(ArrayList<NhanVienDTO> list){
+        nvlist = list;
+    }
     public NhanVien(NhanVienDTO a) throws IOException {
         initcomponent(a);
     }
@@ -189,7 +298,16 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
     @Override
     public void mouseClicked(MouseEvent e) {
        
+       if(e.getSource() == tblnhanvien ){
+           try {
+               selectitem(nvlist);
+           } catch (IOException ex) {
+               Logger.getLogger(NhanVien.class.getName()).log(Level.SEVERE, null, ex);
+           }
+        }
+        
     }
+    
 
     @Override
     public void mousePressed(MouseEvent e) {
@@ -214,6 +332,17 @@ public class NhanVien extends  JPanel implements MouseListener,ActionListener{
     public void actionPerformed(ActionEvent e) {
         
     }
+    public void keyTyped(KeyEvent e) {
+    }
+
+    
+    public void keyPressed(KeyEvent e) {
+    }
+
+    public void keyReleased(KeyEvent e) {
+        desplaydetails(tblnhanvien.getSelectedRow());
+    }
+   
     
     
 }
