3b795723102a0c80a7b1d28825740e04a4c62753
ngvlong <130909111+ngvlong@users.noreply.github.com>
2023-05-13 17:13:33 +0700

    Update PhieuBUS.java

--- src/BUS/PhieuBUS.java
+++ src/BUS/PhieuBUS.java
@@ -7,17 +7,48 @@ package BUS;
 import DTO.PhieuDTO;
 import DAO.PhieuDAO;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 
 
 public class PhieuBUS {
     ArrayList<PhieuDTO> listphieu;
     PhieuDAO phieuDAO = new PhieuDAO();
-    
+    ArrayList<PhieuDTO> phieunhaplist = new ArrayList<>();
+    ArrayList<PhieuDTO> phieuxuatlist = new ArrayList<>();
+
     public PhieuBUS(){
         this.listphieu = phieuDAO.selectAll();
+        phieunhaplist = phieuDAO.selectphieunhap();
     }
-    public ArrayList<PhieuDTO> getlistPhieu(){
+
+    public ArrayList<PhieuDTO> getListphieu() {
         return listphieu;
     }
+
+    public ArrayList<PhieuDTO> getPhieunhaplist() {
+        return phieunhaplist;
+    }
+    
+
+    public ArrayList<PhieuDTO> getPhieuxuatlist() {
+        return phieuxuatlist;
+    }
+    
+    public void addPhieuNhap(PhieuDTO phieu){
+        if(phieuDAO.insert(phieu)!=0){
+            listphieu.add(phieu);
+            phieunhaplist.add(phieu);
+            JOptionPane.showMessageDialog(null, "Thêm thành công");
+        }
+    }
+    
+    public void delPhieuNhap(PhieuDTO phieu){
+        if(phieuDAO.delete(phieu)!=0){
+            listphieu.remove(phieu);
+            phieunhaplist.remove(phieu);
+            JOptionPane.showMessageDialog(null, "Hủy phiếu thành công");
+        }
+    }
+    
 }
