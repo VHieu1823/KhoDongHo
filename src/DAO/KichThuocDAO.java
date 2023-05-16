c5238da410361dda9b11a6da311be234cdebf3c9
ngvlong <130909111+ngvlong@users.noreply.github.com>
2023-05-17 00:39:23 +0700

    Update KichThuocDAO.java

--- src/DAO/KichThuocDAO.java
+++ src/DAO/KichThuocDAO.java
@@ -7,6 +7,7 @@ package DAO;
 import ConnectDatabase.JDBCUtil;
 import DTO.KichThuocDTO;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -19,7 +20,24 @@ public class KichThuocDAO implements interfaceDAO<KichThuocDTO>{
 
     @Override
     public int insert(KichThuocDTO t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int ketQua = 0;
+        JDBCUtil dtb = new JDBCUtil();
+        Connection conn = dtb.openConnection();
+        try {           
+            String sql = "INSERT INTO kichthuoc (kichthuoc) VALUES (?)";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, t.getKichthuoc());
+
+           
+           
+            ketQua = pst.executeUpdate();
+            
+            JDBCUtil.closeConnection(conn);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return ketQua;
     }
 
     @Override
@@ -29,7 +47,23 @@ public class KichThuocDAO implements interfaceDAO<KichThuocDTO>{
 
     @Override
     public int delete(KichThuocDTO t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         int ketQua = 0;
+        JDBCUtil dtb = new JDBCUtil();
+        Connection conn = dtb.openConnection();
+        String sql = "DELETE FROM kichthuoc where kichthuoc=?";
+        try {       
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, t.getKichthuoc());
+            
+            ketQua = pst.executeUpdate();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        dtb.closeConnection(conn);
+        return ketQua;
+    
     }
 
     @Override
