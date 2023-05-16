b79522df4216dd7008f1060eee2acd9d8a525ce0
ngvlong <130909111+ngvlong@users.noreply.github.com>
2023-05-17 00:23:42 +0700

    Update ChatlieuBUS.java

--- src/BUS/ChatlieuBUS.java
+++ src/BUS/ChatlieuBUS.java
@@ -6,7 +6,9 @@ package BUS;
 
 import DAO.ChatlieuDAO;
 import DTO.ChatLieuDTO;
+import DTO.NhaCungCapDTO;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,18 +18,56 @@ public class ChatlieuBUS {
     ChatlieuDAO chatlieudao = new ChatlieuDAO();
     String[] chatlieumatlist = new String[chatlieudao.getslmat()];
     String[] chatlieuvolist = new String[chatlieudao.getslcl()];
+    ArrayList<ChatLieuDTO> chatlieulist = new ArrayList<>();
+    
 
     public ChatlieuBUS() {
         this.chatlieumatlist = chatlieudao.selectmat();
         this.chatlieuvolist = chatlieudao.selectcl();
+        chatlieulist = chatlieudao.selectAll();
     }
 
+    public ArrayList<ChatLieuDTO> getChatlieulist(){
+        chatlieulist = chatlieudao.selectAll();
+        return chatlieulist;
+    }
     public String[] getChatlieumatlist() {
         return chatlieumatlist;
     }
 
     public String[] getChatlieuvolist() {
         return chatlieuvolist;
+    }
+    public int add(ChatLieuDTO cldto){
+        int check =0;
+        int success = 0;
+            for(ChatLieuDTO cl : chatlieulist ){
+            if(cl.getChatlieu().equals(cldto.getChatlieu()) ){
+                JOptionPane.showMessageDialog(null, "Chất liệu đã tồn tại"); 
+                check =1;
+                success = 0;
+            }         
+            }
+            if( check == 0){
+                if(chatlieudao.insert(cldto)!=0){
+                    chatlieulist.add(cldto);
+                    success = 1;
+                    JOptionPane.showMessageDialog(null, "Thêm thành công");
+                }else{
+                    success = 0;
+                    JOptionPane.showMessageDialog(null, "Thêm không thành công");
+            }
+        }
+        return success;
+    }
+    
+    
+    public void del(ChatLieuDTO cl){
+        if(chatlieudao.delete(cl)!=0){
+              chatlieulist.remove(cl);
+               JOptionPane.showMessageDialog(null, "Xóa thành công");
+         }else{
+                JOptionPane.showMessageDialog(null, "Xóa không thành công");}
     }
 
     
