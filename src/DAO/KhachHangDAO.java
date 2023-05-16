5e184c220fec4de00b20510c8c7c832d848f0b4d
ngvlong <130909111+ngvlong@users.noreply.github.com>
2023-05-17 00:38:44 +0700

    Update KhachHangDAO.java

--- src/DAO/KhachHangDAO.java
+++ src/DAO/KhachHangDAO.java
@@ -18,9 +18,28 @@ import ConnectDatabase.JDBCUtil;
  */
 public class KhachHangDAO implements interfaceDAO<KhachHangDTO>{
 
-    @Override
+       @Override
     public int insert(KhachHangDTO t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+      int ketQua = 0;
+        JDBCUtil dtb = new JDBCUtil();
+        Connection conn = dtb.openConnection();
+        try {           
+            String sql = "INSERT INTO khachhang (MaKH,TenKH,SDT,TongTien) VALUES (?,?,?,?)";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, t.getMaKH());
+            pst.setString(2, t.getTenKh());
+            pst.setString(3, t.getSDT());
+            pst.setString(4, t.getTongTien());
+      
+            ketQua = pst.executeUpdate();
+            
+            JDBCUtil.closeConnection(conn);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+        return ketQua;
     }
 
     @Override
@@ -47,9 +66,24 @@ public class KhachHangDAO implements interfaceDAO<KhachHangDTO>{
 
     @Override
     public int delete(KhachHangDTO t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int ketQua = 0;
+        JDBCUtil dtb = new JDBCUtil();
+        Connection conn = dtb.openConnection();
+        String sql = "DELETE FROM khachhang where MaKH=?";
+        try {           
+            
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, t.getMaKH());
+            
+            ketQua = pst.executeUpdate();
+            
+            JDBCUtil.closeConnection(conn);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return ketQua;    
     }
-
     @Override
     public ArrayList<KhachHangDTO> selectAll() {
         ArrayList<KhachHangDTO> khachhang_data = new ArrayList<>();
